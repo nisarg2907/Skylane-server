da232b1f2160bfcdf9557e90b27cf0020e69de71
Nisarg <nisargshah.ict19@gmail.com>
2025-03-26 00:30:49 +0530

    CORS test

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3001;
-  const origin = configService.get<string>('CLIENT_URL');
 
   // Global validation pipe
   app.useGlobalPipes(
@@ -31,7 +30,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: origin,
+    origin: '*',
   });
 
   await app.listen(port);
